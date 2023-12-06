e963115cb986c132c2ec896d07725f498185330b
TayeBay <72499920+Taye20@users.noreply.github.com>
2023-12-06 22:58:43 +0000

    Update Exploratory Analysis.R

--- Exploratory Analysis.R
+++ Exploratory Analysis.R
@@ -1,3 +1,4 @@
+###Exploratory analysis
 require(pacman)
 p_load(sf, PrevMap, tidyverse, tmap, geoR, readr, readxl, car, dplyr, Rmisc, stargazer, mfx) ############ you do not have to install everything.
 
@@ -9,12 +10,11 @@ data <- read_csv("CleandataMBG.csv")
 
 names(data)
 
-
-d2 <- glm(octi ~ ., data = data, family = "binomial")
+d2 <- glm(Mal_Outcome ~ ., data = data, family = "binomial")
 
 summary(d2)
 
-d3 <- glm(data$octi ~ data$WaterSource + data$Bed_net_Usg + data$Win_Protctn, family = "binomial")
+d3 <- glm(data$Mal_Outcome ~ data$WaterSource + data$Bed_net_Usg + data$Win_Protctn, family = "binomial")
 
 summary(d3)
 
@@ -27,7 +27,7 @@ exp(cbind("Odds ratio" = coef(d3), confint.default(d3, level = 0.95)))
 
 
 # Using the --mfx-- package to derive the OR
-AOR <- logitor(data$octi ~ data$WaterSource + data$Bed_net_Usg + data$Win_Protctn, data = data)
+AOR <- logitor(data$Mal_Outcome ~ data$WaterSource + data$Bed_net_Usg + data$Win_Protctn, data = data)
 AOR
 
 
@@ -37,23 +37,19 @@ RiskRatio # note, the Risk ratio is the Odds ratio (AOR)
 
 
 ##### BIVARIATE ANALYSIS ########################
-d4 <- glm(data$octi ~ data$WaterSource, data = data, family = "binomial")
+d4 <- glm(data$Mal_Outcome ~ data$WaterSource, data = data, family = "binomial")
 
 summary(d4)
 
 exp(cbind("Odds ratio" = coef(d4), confint.default(d4, level = 0.95)))
 
-
-
 # Using the --mfx-- package to derive the OR
-OR4 <- logitor(data$octi ~ data$WaterSource, data = data)
+OR4 <- logitor(data$Mal_Outcome ~ data$WaterSource, data = data)
 OR4
 
 
-
-
 ### BEDNET
-d5 <- glm(data$octi ~ data$Bed_net_Usg, data = data, family = "binomial")
+d5 <- glm(data$Mal_Outcome ~ data$Bed_net_Usg, data = data, family = "binomial")
 
 summary(d5)
 
@@ -65,12 +61,12 @@ exp(cbind("Odds ratio" = coef(d5), confint.default(d5, level = 0.95)))
 confint(d5)
 
 # Using the --mfx-- package to derive the OR
-OR5 <- logitor(data$octi ~ data$Bed_net_Usg, data = data)
+OR5 <- logitor(data$Mal_Outcome ~ data$Bed_net_Usg, data = data)
 OR5
 
 
 ### WINDOW PRTCN
-d6 <- glm(data$octi ~ data$Win_Protctn, data = data, family = "binomial")
+d6 <- glm(data$Mal_Outcome ~ data$Win_Protctn, data = data, family = "binomial")
 
 summary(d6)
 
@@ -82,5 +78,5 @@ exp(cbind("Odds ratio" = coef(d6), confint.default(d6, level = 0.95)))
 confint(d6)
 
 # Using the --mfx-- package to derive the OR
-OR6 <- logitor(data$octi ~ data$Win_Protctn, data = data)
+OR6 <- logitor(data$Mal_Outcome ~ data$Win_Protctn, data = data)
 OR6
